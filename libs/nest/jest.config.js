0320eefd188bf531e9f6a4783228cfe2441fa6e4
Jesper Dahlgren <jesper@dahlgrenarna.se>
2021-05-18 09:01:56 +0200

    Merged nest libs to single lib

--- libs/nest/jest.config.js
+++ libs/nest/jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  name: "nest-auth",
-  preset: "../../../jest.config.js",
+  name: "nest",
+  preset: "../../jest.config.js",
   transform: {
     "^.+\\.[tj]sx?$": "ts-jest"
   },
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "html"],
-  coverageDirectory: "../../../coverage/libs/nest/auth"
+  coverageDirectory: "../../coverage/libs/nest"
 };
