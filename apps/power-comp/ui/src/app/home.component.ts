11d522e266dabcd26d5ee67daa03c7179b40d21d
Jesper Dahlgren <jesper@dahlgrenarna.se>
2021-05-25 09:26:34 +0200

    Added link to source

--- apps/power-comp/ui/src/app/home.component.ts
+++ apps/power-comp/ui/src/app/home.component.ts
@@ -9,8 +9,21 @@ import { Environment } from "@pc/power-comp/shared";
       <h1>{{ "welcome.title" | translate }}</h1>
       <div>
         <p>{{ "welcome.author" | translate }}: Jesper Dahlgren</p>
+        <div>
+          <a
+            mat-button
+            href="https://github.com/J-Dahlgren/Power-Comp"
+            target="_blank"
+          >
+            {{ "welcome.source-code" | translate }}
+            <img src="assets/GitHub-Mark-32px.png"
+          /></a>
+        </div>
       </div>
-      <div *ngIf="displayIp && ipAdresses$ | async as ipAdresses">
+      <div
+        *ngIf="displayIp && ipAdresses$ | async as ipAdresses"
+        style="margin-top: 20px;"
+      >
         <h3>{{ "welcome.ip-notice" | translate }}</h3>
         <a
           *ngFor="let ip of ipAdresses"
